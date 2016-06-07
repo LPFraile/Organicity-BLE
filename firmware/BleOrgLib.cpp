c8c692538330509e4149f9e10f931e8d84374c85
LPFraile <lidia_bas@hotmail.com>
2016-06-07 12:46:40 +0300

    Update BleOrgLib.cpp

--- firmware/BleOrgLib.cpp
+++ firmware/BleOrgLib.cpp
@@ -9,6 +9,8 @@ File: source file
 #include "BleOrgLib.h"
 unsigned char buf[16]={0};
 static byte buf_len=0;
+int LED = 13;
+
 BleOrgLib::BleOrgLib()
 {
     index=0;
@@ -23,71 +25,104 @@ void BleOrgLib::orgFunctions(){
 
 void BleOrgLib::orgSetup()
 {
-  analogReference(DEFAULT);
-  sensorSetup();
-  ble_set_name(device_name);
+Serial.println("power on library!");	
+ 
+ pinMode(LED, OUTPUT);
+ digitalWrite(LED, HIGH);
+ ble_set_name(device_name); 		
+ sensorSetup();
+ 
   index=0;
-
   // Init. and start BLE library.
   ble_begin();
- 
-  sensorLoop(); 
 }
 
 void BleOrgLib::orgRun()
 { 
   
   if (ble_connected() )
-  {  
-    uint8_t temp;
-    while ( ble_available() )
-    {
-      noInterrupts();	
-      temp = ble_read();
-	
-      if('V' == temp)
-      {
-        temp = ble_read();
-        if('n' == temp){
-         
-          Serial.println("ReceiveVn");
-          Serial.print("Index");
-          Serial.println(index);
-
-          SendVariables();
-        }   
-      } 
-    } 
+  { 
+	ble_write('G');
+    	bool follow=true;  
+    	double  timer=millis();
+      
+    	while(ble_connected()&&follow)
+	{   
+       		uint8_t temp;
+       		Serial.println("ble conected");
+       		double timer2=millis();
+       		bool sending=true; 
+      		while (ble_available()&&sending)
+      		{
+        		digitalWrite(LED, LOW);  
+        		temp = ble_read();
+        		if('V' == temp)
+        		{
+          			temp = ble_read();
+          			if('n' == temp){
+            
+           				Serial.println("ReceiveVn");
+          				Serial.print("Index");
+              				Serial.println(index);
+               				SendVariables();
+            			}   
+        		 } 
+        		if ((millis()-timer2)>30000)
+         		{
+				Serial.println("stack sending sensor value disconnect for reset the conexion");
+				delay (5000); 
+				ble_disconnect(); 
+				delay(150);
+				sending=false;
+				follow=false;  
+				digitalWrite(LED, HIGH); 
+        		 }
+       		}
+        	ble_do_events();
+        	delay(150);
+	       if ((millis()-timer)>30000)
+	       {
+			  Serial.println("stack waiting for a message disconnect for reset the conexion ");
+			  delay (10000);
+			  ble_disconnect(); 
+			  delay(150);
+			  follow=false;   
+	       }
+   	 }   
   }
-  else
+  else //very importan read only when is not blouthout conection to not stop the protocol comuniation
   {
-    sensorLoop();
+   	sensorLoop();
    }
-  ble_do_events();
+ble_do_events();	
 }
 
 void BleOrgLib::SendVariables()
 {
   
-  //noInterrupts();
-  delay(100);  //jitters elimination
-  if (index<num_var)
-  {
-    Serial.print("Value:");
-    Serial.print(variables_name[index]);
-    Serial.println(variables_value[index]);
-    SendfFloatVariable(variables_name[index],variables_value[index]);
-    index++;
-  }
-  else if(index==num_var)
-  {
-    index=0;
-    ble_write('F');
-    sensorReset();
-  }
+    noInterrupts();
 
-  interrupts();
-
+    delay(100);  //jitters elimination
+    
+    if (index<num_var)
+    {
+	Serial.print("Value:");
+	Serial.print(variables_name[index]);
+      	Serial.println(variables_value[index]);
+      	SendfFloatVariable(variables_name[index],variables_value[index]);
+        index++;
+    }
+    else if(index==num_var)
+    {
+      index=0;
+      delay(150);
+      digitalWrite(LED, HIGH);
+      delay(150);
+      ble_disconnect();
+      sensorReset();	
+    }
+   
+    interrupts();
 }
 void BleOrgLib::ble_write_string(byte *bytes, uint8_t len)
 {
@@ -117,9 +152,12 @@ void BleOrgLib::SendfFloatVariable(String name,float floatVar)
 
   buf[0]='V';
   byte * b = (byte *) &floatVar;
-  int i;
+  int i=0;
   int n= name.length();
   buf[1]=n;
+  Serial.print("name:");
+  Serial.println(name);
+
   for (i=0;i<n;i++)
   {
     buf[i+2]=name[i];
@@ -137,3 +175,4 @@ void BleOrgLib::SendfFloatVariable(String name,float floatVar)
   buf_len = 0;
 
 }
+
